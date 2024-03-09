9abfb88a33def8499a58540c6c555b806e81613f
yash <yashwanthmendu@gmail.com>
2024-03-09 14:07:27 +0530

    changed the color again

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -155,7 +155,7 @@ const CoinsTable = () => {
                             <span
                                 style={{
                                   textTransform: "uppercase",
-                                  color:"black",
+                                  color:"white",
                                   fontSize: 22,
                                 }}
                             >
